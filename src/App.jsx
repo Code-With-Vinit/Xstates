0ee3238767110c68597fe4915cae41ec23a8741b
Code-With-Vinit <122153636+Code-With-Vinit@users.noreply.github.com>
2025-05-15 21:32:04 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,106 @@
+import {useState,useEffect} from "react";
+
+
+
+
+
+
+function App() {
+
+
+    const [countries,setCountries]=useState([]);
+    const [selectedCountry,setSelectedCountry]=useState("");
+    const [selectedState,setSelectedState]=useState("");
+    const [selectedCity,setSelectedCity]=useState("");
+    const [state,setState]=useState([]);
+    const [city,setCity]=useState([]);
+    
+    const COUNTRYENDPOINT="https://crio-location-selector.onrender.com/countries";
+    const STATEENDPOINT=`https://crio-location-selector.onrender.com/country=${selectedCountry}/states`;
+    const CITYENDPOINT=`https://crio-location-selector.onrender.com/country=${selectedCountry}/state=${selectedState}/cities`;
+
+    useEffect(()=>{
+      fetch(COUNTRYENDPOINT).then((response)=>response.json()).then((data)=>{
+        setCountries(data);
+      }).catch((error)=>console.error("Error fetching data: ",error))
+    },[])
+
+    useEffect(()=>{
+      fetch(STATEENDPOINT).then((response)=>response.json()).then((data)=>{
+        setState(data);
+      }).catch((error)=>console.error("Error fetching data: ",error))
+    },[selectedCountry])
+
+    useEffect(()=>{
+      fetch(CITYENDPOINT).then((response)=>response.json()).then((data)=>{
+        setCity(data);
+      }).catch((error)=>console.error("Error fetching data: ",error))
+    },[selectedCountry, selectedState])
+
+
+    
+
+    const handleCountry=(e)=>{
+       setSelectedCountry(e.target.value);
+    }
+
+    const handleState=(e)=>{
+       setSelectedState(e.target.value);
+    }
+
+    const handleCity=(e)=>{
+       setSelectedCity(e.target.value);
+    }
+
+   
+
+
+  return (
+    <>
+      <select style={{width:"350px", height:"25px",margin:"30px"}} onChange={e=>handleCountry(e)}>
+        <option value="someOption">Select Country</option>
+        {
+          countries && countries!==undefined?
+          countries.map((ctr)=>{
+            return(
+            <option key={ctr} value= {ctr}>{ctr}</option>
+            )
+          }):"No Country"
+        }   
+      </select>
+      
+
+      <select style={{width:"350px", height:"25px",margin:"30px"}} disabled={!selectedCountry} onChange={e=>handleState(e)} >
+        <option value="someOption" >Select State</option>
+        {
+          state && state!==undefined?
+          state.map((ctr)=>{
+            return(
+            <option key={ctr} value={ctr}>{ctr}</option>
+            )
+          }):"No State"
+        }   
+      </select>
+     
+
+      <select style={{width:"350px", height:"25px",margin:"30px"}} disabled={!selectedState} onChange={e=>handleCity(e)}>
+        <option value="someOption">Select City</option>
+        {
+          city && city!==undefined?
+          city.map((ctr)=>{
+            return(
+            <option key={ctr} value={ctr}>{ctr}</option>
+            )
+          }):"No City"
+        }   
+      </select>
+
+        {(selectedCountry && selectedState && selectedCity)? 
+          <h3 style={{textAlign:"center"}}>You selected {selectedCountry}, {selectedState}, {selectedCity}</h3>:""
+        }
+
+    </>
+  )
+}
+
+export default App
